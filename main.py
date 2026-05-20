668751b14d57a14ba555a894fe8c790ba97a1665
AidanFisch <aidan.fisch@gmail.com>
2026-05-20 20:37:52 +1000

    Allow localhost:8080 in CORS origins

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ app.add_middleware(
         "http://www.wealthmodel.io",
         "http://localhost:5500",
         "http://127.0.0.1:5500",
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
     ],
     allow_credentials=True,
     allow_methods=["*"],
